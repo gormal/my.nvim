fd766706837bee1373a6fa719878355341d44dd3
gormal <kotas24@gmail.com>
2024-09-09 16:45:24 +0200

    keymaps change

--- init.lua
+++ init.lua
@@ -111,7 +111,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
+vim.keymap.set({ 'n', 't' }, '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+vim.keymap.set({ 'n', 't' }, '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+vim.keymap.set({ 'n', 't' }, '<C-Right>', '<cmd>vertical resize -2<cr>', { desc = 'Increase Window Width' })
+vim.keymap.set({ 'n', 't' }, '<C-Left>', '<cmd>vertical resize +2<cr>', { desc = 'Decrease Window Width' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -12,25 +12,15 @@ return {
       local float_term = Terminal:new { direction = 'float' }
 
       -- Key mappings to toggle each terminal
-      vim.keymap.set('n', '<leader>th', function()
-        horizontal_term:toggle()
-      end, { noremap = true, silent = true, desc = '[H]orrizontally split terminal' })
-      vim.keymap.set('n', '<leader>tv', function()
-        vertical_term:toggle()
-      end, { noremap = true, silent = true, desc = '[V]ertically split terminal' })
+      vim.keymap.set('n', '<leader>td', function()
+        horizontal_term:toggle(5)
+      end, { noremap = true, silent = true, desc = 'Toggle terminal [D]own' })
+      vim.keymap.set('n', '<leader>tr', function()
+        vertical_term:toggle(80)
+      end, { noremap = true, silent = true, desc = 'Toggle terminal [Right]' })
       vim.keymap.set('n', '<leader>tf', function()
         float_term:toggle()
       end, { noremap = true, silent = true, desc = '[F]loating terminal' })
-
-      -- Map <ESC><ESC> in terminal mode to close the terminal
-      function _G.close_term_on_double_esc()
-        local term_id = vim.b.toggle_number
-        if term_id then
-          require('toggleterm.terminal').get(term_id):toggle()
-        end
-      end
-
-      vim.keymap.set('t', '<esc><esc>', '<C-\\><C-n>:lua close_term_on_double_esc()<CR>', { noremap = true, silent = true })
     end,
     config = function() end,
   },
